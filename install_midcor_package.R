bcf635a6f58b08caa7afc83c0b86fe9c702a6317
Vitaly Selivanov <selivanov@ub.edu>
2016-06-01 13:02:40 +0200

    changed R version in description

--- install_midcor_package.R
+++ install_midcor_package.R
@@ -3,3 +3,23 @@
 library(devtools)
 build()
 install()
+******************-------------------------******************
+# run script:
+vs@vit:~/phenomen/docker-midcor/scripts$ ./runMidcor.R -i ~/phenomen/outin.csv -o ~/phenomen/tes_out.csv
+
+******************-------------------------******************
+
+# crear Docker container:
+
+# go to the directory where the dockerfile is
+cd /home/vs/phenomen/docker-midcor
+# create container from dockerfile
+sudo docker build -t midcor:0.1 .
+
+# check existing images:
+sudo docker images
+
+# try to run the tool from docker
+sudo docker run --name=midcor-d -i -t midcor:0.1 -i outin.csv -o my_out.csv 
+
+sudo docker run -i -t -v $PWD:/data midcor:0.1 -i /data/outin.csv -o /data/my_out.csv
