04649aad95d739e1f4f1ab2203907901bf1bf5cb
Vitaly <selivanov@ub.edu>
2017-07-17 11:06:23 +0200

    corrected convert()

--- R/lib.R
+++ R/lib.R
@@ -103,20 +103,53 @@ convert<-function(rdat,iln){
          }
          met<-rdat[iln,colmet];
          frag<-rdat[iln,colfrg];
-        
-           i<-iln+1;  id<-as.character(rdat[i,colid]); numln<-nrow(rdat)
-           rdat[,coliso]<-as.character(rdat[,coliso])
-            first<-0; m<-numeric(); 
-            
-      while(i<nrow(rdat)){
-      while(grepl("13C-1",rdat[i,coliso]) | rdat[i,coliso]=="") i<-i+1
-      if(grepl("13C",rdat[i,coliso])){    k<-1;
-       while(as.integer(strsplit(rdat[i,coliso],"13C")[[1]][2]) >= 0) {
-               m[k]=as.numeric(as.character(rdat[i,coldis])); i<-i+1; if(!grepl("13C",rdat[i,coliso])) break; k<-k+1;}
-                           first<-first+1;
-                          if(first==1) { lm<-length(m); mm<-matrix(nrow=1,ncol=lm); mm[1,]<-m;}
-                           else mm<-rbind(mm,m)
-                            }
-      }
-           return(list(id,mm,i,colmet,nCder,nCfrg,nSi))
-}
+         m<-numeric();     k<-1;
+           i<-iln+1;  id<-as.character(rdat[i,colid]);
+       while(!grepl("C-1",rdat[i,coliso])) {# distribution of intensities for 1st injection
+         if(frag==rdat[i,colfrg]){
+               m[k]=as.numeric(as.character(rdat[i,coldis])); i=i+1; k=k+1}
+         else i=i+1 }
+       lm<-length(m)
+       mm<-matrix(nrow=1,ncol=lm); mmm=mm # mm - for all inj; mmm - for summed inj
+        mm[1,]=m; m1=m                    # m - for all inj; m1 - for summed inj 
+       while(met==rdat[i,colmet]){  #  metabolite cycle
+        id<-c(id,as.character(rdat[i,colid]))
+            m<-numeric(lm); k<-1
+            i=i+1
+       while(!grepl("C-1",rdat[i,coliso])) {  #  replicate cycle
+          if(frag==rdat[i,colfrg]){
+             m[k]=as.numeric(as.character(rdat[i,coldis]));
+             i=i+1;  k=k+1 }
+           else { i=i+1 }
+       if(i>nrow(rdat)) { break}}
+       if(lm==length(m)) {mm=rbind(mm,m)};
+       if(i>nrow(rdat)) {break}}
+       return(list(id,mm,i,colmet,nCder,nCfrg,nSi))
+}       
+#convert<-function(rdat,iln){
+#        colid=1; a=ginfo(rdat,iln);colmet=0;
+#         nCder=a[[1]]; nCfrg=a[[2]]; nSi=a[[3]]; colfrg=a[[4]];
+#  for(i in 1:ncol(rdat)){if(grepl("signal intens",rdat[1,i])) {coldis=i} # column of signal intensity
+#               else {if(grepl("Metab",rdat[1,i])) colmet=i;} #  column of metabolite name
+#         if (grepl("isotopolog",rdat[1,i])) {coliso=i;}
+#         if (grepl("labelled pos",rdat[1,i])) colab=i
+#         }
+#         met<-rdat[iln,colmet];
+#         frag<-rdat[iln,colfrg];
+#        
+#           i<-iln+1;  id<-as.character(rdat[i,colid]);
+#           rdat[,coliso]<-as.character(rdat[,coliso])
+#            first<-0; m<-numeric(); 
+#            
+#      while(i<nrow(rdat)){
+#      while(grepl("13C-1",rdat[i,coliso]) | rdat[i,coliso]=="") i<-i+1
+#      if(grepl("13C",rdat[i,coliso])){    k<-1
+#       while(as.integer(strsplit(rdat[i,coliso],"13C")[[1]][2]) >= 0) {
+#               m[k]=as.numeric(as.character(rdat[i,coldis])); i<-i+1; if(!grepl("13C",rdat[i,coliso])) break; k<-k+1;}
+#                           first<-first+1;
+#                          if(first==1) { lm<-length(m); mm<-matrix(nrow=1,ncol=lm); mm[1,]<-m;}
+#                           else mm<-rbind(mm,m)
+#                            }
+#      }
+#           return(list(id,mm,i,colmet,nCder,nCfrg,nSi))
+#}
